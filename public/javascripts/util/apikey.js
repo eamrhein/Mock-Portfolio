0603a57fec1e99c5f50d298b56a1b1b589edff98
Eric Amrhein <eamrhein@protonmail.ch>
2019-10-07 14:58:08 -0700

    fixes stuff

--- public/javascripts/util/apikey.js
+++ public/javascripts/util/apikey.js
@@ -1,5 +1,5 @@
 module.exports = {
-  iexkey: 'pk_5b573d37d6e34cedaf9b10461bd54c43',
+  iexkey: 'pk_a19e669c763243e49bb5f82fc313f096',
 };
 //iexkey: 'pk_5b573d37d6e34cedaf9b10461bd54c43',
 // iexkey: 'pk_9c1ed2a08a2c4af2be14db7a6c97d602',

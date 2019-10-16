4efb07c91f7cafba51e74e84f57a28ac066eff91
Eric Amrhein <eamrhein@protonmail.ch>
2019-10-15 22:51:27 -0700

    stock-ticker

--- public/javascripts/components/stockticker.js
+++ public/javascripts/components/stockticker.js
@@ -1,1 +1,27 @@
+/* eslint-disable require-jsdoc */
 import Component from '../lib/component.js';
+import store from '../store/index.js';
+import {getQuotes} from '../util/api-calls';
+import {stocks} from '../index';
+export default class StockTicker extends Component {
+  constructor() {
+    super({
+      store: store,
+      element: document.getElementById('ticker'),
+    });
+  }
+  updateTicker() {
+    setInterval((e) => {
+      getQuotes(stocks).then((data) => {
+        console.log(data);
+        store.dispatch('updateTicker', data);
+      });
+    }, 9000);
+  }
+  render() {
+    const {ticker} = store.state;
+    ticker.forEach((stock) => {
+      this.element.innerHTML+= `<span class=${stock.diff > 0 ? 'green' : 'red'}>${stock.symbol} ${stock.price} ${stock.diff}</span>`;
+    });
+  }
+};
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,36 +3,38 @@ import store from './store/index.js';
 import {fetchAllHist, fetchAllInfo, getQuotes} from './util/api-calls';
 // Load up components
 import Portfolio from './components/portfolio';
+import StockTicker from './components/stockticker';
+
+export const stocks = [
+  {
+    sym: 'TSLA',
+    shares: 10,
+    name: 'Tesla Motors, Inc.',
+  },
+  {
+    sym: 'GOOG',
+    shares: 4,
+    name: 'Google Inc.',
+  },
+  {
+    sym: 'MSFT',
+    shares: 24,
+    name: 'Microsoft Corporation',
+  },
+  {
+    sym: 'AAPL',
+    shares: 9,
+    name: 'APPLE Inc.',
+  },
+  {
+    sym: 'AMZN',
+    shares: 3,
+    name: 'Amazon.com Inc.',
+  },
+];
 
 document.addEventListener('DOMContentLoaded', (e) => {
   // DEFALT SETTINGS
-  const stocks = [
-    {
-      sym: 'TSLA',
-      shares: 10,
-      name: 'Tesla Motors, Inc.',
-    },
-    {
-      sym: 'GOOG',
-      shares: 4,
-      name: 'Google Inc.',
-    },
-    {
-      sym: 'MSFT',
-      shares: 24,
-      name: 'Microsoft Corporation',
-    },
-    {
-      sym: 'AAPL',
-      shares: 9,
-      name: 'APPLE Inc.',
-    },
-    {
-      sym: 'AMZN',
-      shares: 3,
-      name: 'Amazon.com Inc.',
-    },
-  ];
   store.dispatch('fetchAllStockSymbols', stockList);
   fetchAllHist(stocks).then((history) => {
     store.dispatch('fetchAllhistory', history);
@@ -41,9 +43,12 @@ document.addEventListener('DOMContentLoaded', (e) => {
       store.dispatch('fetchAllinfo', info);
     }).then(() => {
       getQuotes(stocks).then((data) => {
-        console.log(data);
+        store.dispatch('updateTicker', data);
         const porfolio = new Portfolio();
+        const ticker = new StockTicker();
         porfolio.render();
+        ticker.render();
+        ticker.updateTicker();
       });
     });
   }

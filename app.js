aa8df8f361083f077b92e29a1ee6aa36462b3ec6
Eric Amrhein <eamrhein@protonmail.ch>
2019-12-02 12:05:44 -0800

    more changes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const compression = require("compression");
+const express = require('express');
+const compression = require('compression');
 const app = express();
-const path = require("path");
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+// this is a comment
 app.use(compression());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (request, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get('/', (request, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {

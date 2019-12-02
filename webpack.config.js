6ce9e6e61ba1e106c07a2d6efb61980e1255c4b5
Eric Amrhein <eamrhein@protonmail.ch>
2019-12-02 11:22:39 -0800

    optimize mockportfolio

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 const config = {
-  entry: path.join(__dirname, './public/javascripts/index.js'),
+  entry: path.join(__dirname, "./public/javascripts/index.js"),
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, './public'),
+    filename: "bundle.js",
+    path: path.join(__dirname, "./public")
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    }],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
   },
   plugins: [
     new webpack.ProgressPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new MinifyPlugin()
   ],
-  devtool: 'source-map',
+  devtool: "source-map"
 };
 
 module.exports = config;

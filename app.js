42c1b075b374a9291beab2dc77c54294e02101eb
Eric Amrhein <eamrhein@protonmail.ch>
2019-12-02 11:51:17 -0800

    optimize mockportfolio

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
+const compression = require("compression");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
+app.use(compression());
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-app.get('/', (request, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/", (request, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
